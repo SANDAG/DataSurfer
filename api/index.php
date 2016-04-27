67f5697d9835cf339cac5ff7e25d2d7b91a82417
Park <jpa@sandag.org>
2016-04-27 15:11:27 -0700

    updated acs_avg_travel_time_to_work index

--- api/index.php
+++ api/index.php
@@ -1037,7 +1037,7 @@ $app->get('/:datasource/:series/:geotype/:zone/map/geojson', function ($datasour
 				if (is_array($travelTimetoWorkAverageZoneArray)){
 					
 					foreach($travelTimetoWorkAverageZoneArray as $arr)
-						$travelTimetoWorkArray[$travelTimetoWorkIterator++] = [$arr[$geoType], $arr['yearnumber'], $arr['averagetimetowork'], $arr['avg_travel_time']];
+						$travelTimetoWorkArray[$travelTimetoWorkIterator++] = [$arr[$geoType], $arr['yearnumber'], $arr['averagetimetowork'], $arr['acs_avg_travel_time_to_work']];
 				}
 				//******* 21 Employment Status ***************************************
 				$employmentStatus_file_name = strtolower(join("_", array('employmentStatus', "censusacs", $year, $geoType, $zone)).".json");
