5b7b15cd0c85bc1e358e787cf0dc22fd56b63e94
unknown <cdan@SDG77861.sandag.org>
2017-07-19 16:37:07 -0700

    Update geojson processing script.

--- api/utilities/geojson_processing.py
+++ api/utilities/geojson_processing.py
@@ -8,7 +8,7 @@ password = '48RedSlideq7VbK'
 
 with psycopg2.connect(database=db, user=user, password=password, host=server) as conn:
     with conn.cursor() as cur:
-        with open('e:/data/data_surfer/cities_cpa.geojson') as  j:
+        with open('e:/data/data_surfer/geo2016.json') as  j:
             data = json.load(j)
             features = data["features"]
             for feature in features:
