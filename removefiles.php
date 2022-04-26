c83d793eda1fff4c534b9fa319927559ea0fba8e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-27 06:53:27 +0800

    Delete removefiles.php

--- removefiles.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dir    = 'download';
-$files = scandir($dir);
-foreach($files as $current_files){
-    if(file_exists($dir."/".$current_files)){
-        unlink($dir."/".$current_files);
-    }
-}
-?>
